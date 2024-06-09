33d20cf3030af81a9ff4e6a8828290ce3a102090
Kauan Lauer <143208136+kauanlauer@users.noreply.github.com>
2024-06-09 13:32:29 -0300

    Add files via upload

---
+++ Grupos/grupos.php
@@ -0,0 +1,156 @@
+<?php
+include('../Login/protect.php');
+include('../Login/conexao.php');
+include('../Login/functions.php'); // Inclui o arquivo de funções
+
+// Verificar se o perfil do usuário está completo
+$user_id = $_SESSION['id'];
+$sql_foto = "SELECT foto FROM usuarios WHERE id='$user_id'";
+$result_foto = $mysqli->query($sql_foto);
+$usuario_foto = $result_foto->fetch_assoc();
+$foto = $usuario_foto['foto'] ?? '';
+
+// Obtenha as últimas 5 despesas do usuário
+$despesasMensais = getDespesasMensais($_SESSION['id'], $mysqli, 5); // Limita o número de resultados a 5
+$totalDespesasMensais = getTotalDespesasMensais($_SESSION['id'], $mysqli);
+
+$user_id = $_SESSION['id'];
+$sql = "SELECT nome FROM usuarios WHERE id='$user_id'";
+$result = $mysqli->query($sql);
+$usuario = $result->fetch_assoc();
+$primeiro_nome = explode(' ', $usuario['nome'])[0];
+
+// Obtenha o salário líquido do usuário
+$sql_salario = "SELECT salario_liquido FROM usuarios WHERE id='$user_id'";
+$result_salario = $mysqli->query($sql_salario);
+$usuario_salario = $result_salario->fetch_assoc();
+$salario_liquido = $usuario_salario['salario_liquido'];
+
+// Calcule a diferença entre o salário líquido e as despesas totais
+$diferenca = $salario_liquido - $totalDespesasMensais;
+echo "<script>console.log('Salário Líquido: " . $salario_liquido . "');</script>";
+echo "<script>console.log('Total de Despesas Mensais: " . $totalDespesasMensais . "');</script>";
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto&display=swap">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <title>Menu lateral</title>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <link rel="stylesheet" type="text/css" href="grupos.css">
+    <style>
+            .maintenance {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #f9f9f9;
+    text-align: center;
+}
+
+.maintenance img {
+    width: 1200px;
+    height: auto;
+}
+
+.maintenance p {
+    font-size: 20px;
+    color: #333;
+}
+    </style>
+</head>
+<body>
+
+    <nav class="menu-lateral"> <!-- menu lateral-->
+
+        <div class="bt-expandir">
+            <i class="bi bi-list" id="btn-exp"></i>
+        </div>
+
+        <ul>
+        <li class="item-menu">
+            <a href="../Usuario/painel.php">
+                <span class="icon"><i class="bi bi-info-circle"></i></span>
+                <span class="txt-link">Painel</span>
+            </a>
+        </li>
+        <li class="item-menu">
+            <a href="../Usuario/painel_despesas.php">
+                <span class="icon"><i class="bi bi-piggy-bank"></i></span>
+                <span class="txt-link">Despesas</span>
+            </a>
+        </li>
+        <li class="item-menu">
+            <a href="../Usuario/dashboard.php">
+                <span class="icon"><i class="bi bi-columns-gap"></i></i></span>
+                <span class="txt-link">Dashboard</span>
+            </a>
+        </li>
+        <li class="item-menu ativo">
+            <a href="grupos.php">
+                <span class="icon"><i class="bi bi-people-fill"></i></span>
+                <span class="txt-link">Grupos</span>
+            </a>
+        </li>
+    </ul>
+    </nav> <!-- menu lateral-->
+
+    <!-- Foto do perfil para mostrar na página -->
+<div class="profile-container">
+    <?php if ($foto) : ?>
+        <a href="#" id="profile-link" class="profile-link">
+            <img src="../uploads/<?php echo htmlspecialchars($foto); ?>" alt="Foto do Perfil" class="profile-photo-small">
+            <span class="username"><?php echo htmlspecialchars($primeiro_nome); ?></span>
+        </a>
+    <?php endif; ?>
+</div>
+<!-- Modal de perfil -->
+<div id="profile-modal" class="modal">
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <p><a href="perfil.php" class="modal-link">Acessar Perfil</a></p>
+        <p><a href="../Login/logout.php" id="close-modal" class="modal-link">Sair do Perfil</a></p>
+    </div>
+</div>
+<!-- Foto do perfil para mostrar na página -->
+
+<div class="maintenance"> <!-- Em manutenção -->
+    <img src="../images/image.gif" alt="Site em manutenção" />
+    <p>Desculpe pelo transtorno. Nosso site está atualmente em manutenção.</p>
+</div><!-- Em manutenção -->
+
+    <script src="grupos.js"></script>
+ <script>
+
+// ----------------------- Modal do Perfil -------------------------------------------
+// Função para abrir o modal ao clicar na foto de perfil
+document.getElementById("profile-link").addEventListener("click", function(event) {
+    event.preventDefault(); // Impede o comportamento padrão do link
+    document.getElementById("profile-modal").style.display = "block"; // Exibe o modal
+});
+
+// Função para fechar o modal ao clicar no "X"
+document.getElementsByClassName("close")[0].addEventListener("click", function() {
+    document.getElementById("profile-modal").style.display = "none"; // Oculta o modal
+});
+
+// ----------------------- Modal do Perfil -------------------------------------------
+
+        document.getElementById("openBtn").addEventListener("click", function() {
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("mainContent").style.marginLeft = "250px";
+});
+
+document.getElementById("closeBtn").addEventListener("click", function() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("mainContent").style.marginLeft = "0";
+});
+    </script>
+</body>
+</html>
