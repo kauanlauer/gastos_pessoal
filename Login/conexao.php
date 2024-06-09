c59e9405400a8b6e3ff75cb0a5041a5f8d776d4f
Kauan Lauer <143208136+kauanlauer@users.noreply.github.com>
2024-06-09 14:12:30 -0300

    Update conexao.php

--- Login/conexao.php
+++ Login/conexao.php
@@ -1,12 +1,12 @@
 <?php
-$usuario = 'id22260352_root';  // Atualize o usuário
-$senha = 'Kauan3396@';     // Atualize a senha (coloque a senha fornecida)
-$database = 'id22260352_login'; // Atualize o nome do banco de dados
-$host = 'localhost';           // Atualize o host se necessário (normalmente 'localhost')
+$usuario = 'gastopessoal';
+$senha = 'Kauan3396@';
+$database = 'gasto_pessoal';
+$host = 'gasto-pessoal.mysql.uhserver.com';
 
 $mysqli = new mysqli($host, $usuario, $senha, $database);
 
-if ($mysqli->connect_error) {
-    die("Falha ao conectar ao banco de dados: " . $mysqli->connect_error);
-}
+if ($mysqli->error) {
+    die("Falha ao conectar ao banco de dados: " . $mysqli->error);
+} 
 ?>
